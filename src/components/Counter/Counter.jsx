4474d3d034414092f456f0819fd444b044f3b95c
Hugo <76168685+HPTR@users.noreply.github.com>
2022-06-07 16:15:09 +0100

    Convert button components to elements within counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import Button from "../Button/Button"
+import React, { useState } from 'react'
 import "./Counter.scss"
 
 const Counter = () => {
+
+  const [count, setCount] = useState(0);
+
   return (
     <div className='counter'>
         <h4 className='counter__title'>Counter</h4>
-        <Button className="counter__button counter__button--decrement" />
-        <p className='counter__count'>0</p>
-        <Button className="counter__button counter__button--increment" />
+        <button type="decrement" onClick={() => setCount(count - 1)} className="counter__button counter__button--decrement">-</button>
+        <p className='counter__count'>{count}</p>
+        <button type="increment" onClick={() => setCount(count + 1)} className="counter__button counter__button--increment">+</button>
     </div>
   )
 }
