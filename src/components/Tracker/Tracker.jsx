b1668ef38aa16d44e9729f3a238258ab97cdaebd
Hugo <76168685+HPTR@users.noreply.github.com>
2022-06-07 15:18:07 +0100

    Add JSX to Tracker.jsx

--- src/components/Tracker/Tracker.jsx
+++ src/components/Tracker/Tracker.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
 const Tracker = (props) => {
+
+  const {id, name, role } = props;
+
   return (
+    <div key={id} className='tracker'>
+        <h2 className='tracker__name'>{name}</h2>
+        <h3 className='tracker__role'>{role}</h3>
+        <div className='counter'>
+          <h4>Counter</h4>
+          <p>0</p>
+          <button></button>
+        </div>
+    </div>
   )
 }
 
