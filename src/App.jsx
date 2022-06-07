9e9ab5288fb283dddfde49073284f416ce356ece
Hugo <76168685+HPTR@users.noreply.github.com>
2022-06-07 15:17:57 +0100

    Add JSX to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,3 +2,16 @@ import './App.scss';
 import employees from "./data/employees";
 import Tracker from './components/Tracker/Tracker';
 
+function App() {
+
+
+
+  return (
+    <div className="tracker-container">
+      <h1>Ticket Tracker</h1>
+      {employees.map(employee => <Tracker employee={employee} />)}
+    </div>
+  );
+}
+
+export default App;
