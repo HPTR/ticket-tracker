e3e46f88521db8c7b3e94b25aee69e10401c76e4
Hugo <76168685+HPTR@users.noreply.github.com>
2022-06-08 10:38:23 +0100

    Make counter functions more readable

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -11,12 +11,20 @@ const Counter = () => {
   const plusIcon = <FontAwesomeIcon icon={faPlus} />;
   const minusIcon = <FontAwesomeIcon icon={faMinus} />;
 
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div className='counter'>
         <h4 className='counter__title'>Counter</h4>
-        <button type="decrement" onClick={() => setCount(count - 1)} className="counter__button counter__button--decrement">{minusIcon}</button>
+        <button type="decrement" onClick={decrement} className="counter__button counter__button--decrement">{minusIcon}</button>
         <p className='counter__count'>{count}</p>
-        <button type="increment" onClick={() => setCount(count + 1)} className="counter__button counter__button--increment">{plusIcon}</button>
+        <button type="increment" onClick={increment} className="counter__button counter__button--increment">{plusIcon}</button>
     </div>
   )
 }
