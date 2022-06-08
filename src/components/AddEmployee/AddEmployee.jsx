79683711265053befdb75e8b53a8b81516d0638b
Hugo <76168685+HPTR@users.noreply.github.com>
2022-06-08 16:11:16 +0100

    Add initial files for AddEmployee component

---
+++ src/components/AddEmployee/AddEmployee.jsx
@@ -0,0 +1,10 @@
+import "./AddEmployee.scss";
+import { React, useState } from "react";
+const AddEmployee = (props) => {
+    const plusIcon = <FontAwesomeIcon icon={faPlus} size="6x" />;
+
+  return (
+  )
+}
+
+export default AddEmployee
