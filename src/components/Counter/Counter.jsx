5a383c83c8d25e54f83c4648fa0a397e0f722945
Hugo <76168685+HPTR@users.noreply.github.com>
2022-06-08 14:07:47 +0100

    Prevent counter from going below 0

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -16,12 +16,14 @@ const Counter = () => {
   };
 
   const handleDecrement = () => {
-    setCount(count - 1);
+    if (count !== 0) {
+      setCount(count - 1);
+    }
   };
 
   return (
     <div className='counter'>
-        <h4 className='counter__title'>Counter</h4>
+        <h4 className='counter__title'>Tickets</h4>
         <button type="decrement" onClick={handleDecrement} className="counter__button counter__button--decrement">{minusIcon}</button>
         <p className='counter__count'>{count}</p>
         <button type="increment" onClick={handleIncrement} className="counter__button counter__button--increment">{plusIcon}</button>
