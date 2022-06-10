3f94c4de096f09e78d7f990f0bdef2dddbb9ab37
Hugo <76168685+HPTR@users.noreply.github.com>
2022-06-10 12:20:25 +0100

    Add delete button and logic

--- src/components/Tracker/Tracker.jsx
+++ src/components/Tracker/Tracker.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import "./Tracker.scss"
 import Counter from '../Counter/Counter';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
 
 const Tracker = (props) => {
 
-  const { name, role, initialTickets } = props;
+  const { name, role, initialTickets, removeEmployee } = props;
+
+  const crossIcon = <FontAwesomeIcon icon={faXmark} />
+
+  const handleClick = (event) => {
+    const clickedEmployeeName = event.target.parentElement.parentElement.parentElement.querySelector('.tracker__name').innerText;
+    if (window.confirm('Are you sure you wish to remove this tracker?')) {
+      removeEmployee(state => [...state].filter(employee => employee.name !== clickedEmployeeName));
+    }
+  }
 
   return (
     <div className='tracker'>
+        <button onClick={handleClick} className='tracker__delete' alt="delete">{crossIcon}</button>
         <h2 className='tracker__name'>{name}</h2>
         <h3 className='tracker__role'>{role}</h3>
         <Counter initialTickets={initialTickets} />
